19b83d2302ae330629516b3f5644143df15f36ef
dfbrush <117858443+dfbrush@users.noreply.github.com>
2022-11-09 22:45:58 -0500

    Update text.py

--- text.py
+++ text.py
@@ -2,8 +2,10 @@ import pygame
 from pygame.locals import * 
 from colors import *
 
+#Class that creates and modifys text for start and end screen
 class Text(pygame.sprite.Sprite):
 
+#Design of text and how it will be updated and created
   def __init__(self,pos,text):
     super().__init__()
     self.font = pygame.font.SysFont("Times New Roman",30)
