283a6afaddd6bec03407cd603ed760f0f15ea812
dfbrush <117858443+dfbrush@users.noreply.github.com>
2022-11-09 22:45:24 -0500

    Update colors.py

--- colors.py
+++ colors.py
@@ -1,9 +1,5 @@
-  
+#Four main colors used throughout the program
 WHITE = (255,255,255)
 RED = (255,0,0)
-BLUE = (0,0,255)
-GREEN = (0,255,0)
-LAVENDER = (158, 158, 219)
-BG_COLOR = (0,102,0)
-BLACK = (0,0,0)
 PRUSSIAN_BLUE = (0,49,83)
+BLACK = (0,0,0)
